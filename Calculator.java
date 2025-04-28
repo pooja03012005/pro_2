1c82cf42d94b5dfc9018031ae16c1361ff450bf1
Pooja01032005 <poojamadiwalar449@gmail.com>
2025-04-28 14:18:26 +0530

    hello

---
+++ Calculator.java
@@ -0,0 +1,9 @@
+public class Calculator {
+    public static void main(String[] args) {
+        int a=10,b=5;
+        System.out.println("addition:"+(a+b));
+        System.out.println("subtraction:"+(a-b));
+        System.out.println("multiplication:"+(a*b));
+        System.out.println("division:"+(a/b));
+    }
+}
